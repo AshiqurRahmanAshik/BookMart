db401a075c5d4afd8909ad4ce2b5c0d27f799d77
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-01-15 22:23:45 +0600

    MongoDB database setup added

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,21 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.DB_NAME;
+
+const collections = {
+  PRODUCTS: "products",
+  USERS: "users",
+  ORDERS: "orders",
+};
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const dbConnect = (cname) => {
+  return client.db(dbName).collection(cname);
+};
